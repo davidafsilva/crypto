c100a5b0c84f639251dccc5164c974341ed04431
David Silva <david@davidafsilva.pt>
2015-07-23 23:15:44 +0100

    initial repo commit; added shift cipher implementation

---
+++ README.md
@@ -0,0 +1,2 @@
+# [cryptography @coursera](https://www.coursera.org/course/cryptography) course code
+A simple repository  for code related to the cryptography course at coursera.
---
+++ shiftcipher.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+
+# basic shift cipher implementation
+# @davidafsilva
+from string import ascii_lowercase as cs
+
+# enc(m1..mt, k) := c1..ct
+#   where ci := [mi + k mod 26]
+def enc(m, k):
+    c = ''
+    for cm in m:
+        c += cs[(cs.index(cm) + k) % 26]
+    return c
+
+# dec(c1..ct, k) := m1..mt
+#   where mi := [ci - k mod 26]
+def dec(c, k):
+    m = ''
+    for cc in c:
+        m += cs[(cs.index(cc) - k) % 26]
+    return m
+
+# test the cipher
+print enc("cryptoisfun",25)
+print dec(enc("cryptoisfun",25), 25)
